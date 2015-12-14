11d4230af154edc6d7072aebbc0f198ba1c47c44
Bishnu Sarker <bishnukuet@gmail.com>
2015-12-14 20:39:37 +0100

    new commit

--- Preprocessing/directorypath.py
+++ Preprocessing/directorypath.py
@@ -1,3 +1,22 @@
 # this file contains all the  variables pointing to some directory or file path
-
+import os as OS
+from shutil import copy2
 DATA_DIR="E:\NLP\Data Sets\NLP DataBooks\A\AA - Unice"
+
+source_path=""#"E:\NLP\Data Sets\NLP DataBooks\PDFs\NLPtxt\NLPtxt"
+destination_path=""#"E:\NLP\Data Sets\NLP DataBooks\\novels\\"
+BooksPath=list()
+def getallfiles(Path,dst):
+    dirList=OS.listdir(Path)
+    for deep1path in dirList:
+        deep2path=OS.path.join(Path,deep1path)
+        if OS.path.isdir(deep2path):
+            getallfiles(deep2path,dst)
+        else:
+            BooksPath.append(deep2path)
+            copy2(deep2path,dst)
+
+if __name__ == '__main__':
+    getallfiles(source_path,destination_path)
+
+
